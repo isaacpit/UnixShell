07109e3b807bb787c4c56eeb56805595fdbb8ed3
isaacpitblado <isaacpitblado@tamu.edu>
2021-10-08 23:55:05 -0500

    implemented 2 processes speaking to each other

--- start.cpp
+++ start.cpp
@@ -11,22 +11,85 @@
 using namespace std;
 
 int main (){
+
+
+    // int READ_END = 0;
+    // int READ_END = 1;
+    int fd[2];
+
+    string s = "ps aux | awk '/init/{print $1}' | sort -r";
+
+    char* args1[] = {"ls", "-la", "/", NULL};
+    char* args2[] = {"grep", "dev", NULL};
+    char* echo_args1[] = {"echo", "'test1'", NULL};
+    char* echo_args2[] = {"echo", "'test2'", NULL};
+
+    int P_READ = 0;
+    int P_WRITE = 1;
+
+    int STD_IN = 0;
+    int STD_OUT = 1;
+
     while (true){
-        cout << "My Shell$ ";
+        // cout << "My Shell$ ";
         string inputline;
-        getline (cin, inputline);   // get a line from standard input
+        // getline (cin, inputline);   // get a line from standard input
+        // vector<int[]> arr_pipes;
+
         if (inputline == string("exit")){
             cout << "Bye!! End of shell" << endl;
             break;
         }
-        int pid = fork ();
-        if (pid == 0){ //child process
+        // inputline = args;
+        
+
+        int pid = fork();
+        if (pid == 0){ // child process
             // preparing the input command for execution
-            char* args [] = {(char *) inputline.c_str(), NULL};  
-            execvp (args [0], args);
+            
+            cout << "outer child: " << pid << endl;
+            pipe(fd);
+            cout << "fd: " << fd[0] << ", " << fd[1] << endl;
+            int pid_inner = fork();
+            
+
+            if (pid_inner == 0) {
+                // inner child process
+                // first process : ls -la / 
+                cout << "inner child: " << pid_inner << endl;
+                cout << "\targs: " << args1[0] << endl;
+                dup2(fd[P_WRITE], STD_OUT);
+                close(fd[P_READ]);
+                close(fd[P_WRITE]);
+                cout << "HERE IS MORE" << endl;
+                
+                execvp(args1[0], args1);
+            } else {
+                // inner "parent" process
+                // second process : grep dev
+                cout << "inner parent: " << pid_inner << endl;
+                cout << "\targs: " << args2[0] << endl;
+                waitpid(pid_inner, 0, 0);
+                dup2(fd[P_READ], STD_IN);
+                close(fd[P_READ]);
+                close(fd[P_WRITE]);
+                // string s;
+                // while (getline(cin, s)) {
+                //     cout << s << endl;
+                // }
+                // cout << "AFTER READING STDIN: "<< endl;
+                execvp(args2[0], args2);
+                // exit(0);
+            }
+        
         }else{
+            cout << "parent: " << pid << endl;
             waitpid (pid, 0, 0); // wait for the child process 
+            
             // we will discuss why waitpid() is preferred over wait()
         }
+        // exit(0);
+        getline(cin, inputline);
+        
     }
 }
